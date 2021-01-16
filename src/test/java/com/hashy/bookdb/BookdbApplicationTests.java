12bb9e83392d7799df455d0ec1e34d2aceab352b
hasanyesil <hasannyesil97@gmail.com>
2021-01-16 15:21:25 +0300

    Initial add

---
+++ src/test/java/com/hashy/bookdb/BookdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashy.bookdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
